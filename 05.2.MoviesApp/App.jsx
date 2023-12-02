1bd73ec4f31f2e0d16663a244e107f92c9bf704a
kemeldev <kemel.developer@gmail.com>
2023-12-02 11:30:32 -0600

    05.2 Project completed

--- 05.2.MoviesApp/App.jsx
+++ 05.2.MoviesApp/App.jsx
@@ -1,21 +1,22 @@
-import { useState, useRef, useEffect } from 'react'
+import { useState, useRef, useEffect, useMemo, useCallback } from 'react'
 import noResponseJSON from './mocks/noResponse.json'
+import debounce from 'just-debounce-it'
 
 function useSearch () {
-  const [search, setSearch] = useState(null)
+  const [search, setSearch] = useState('')
   const [error, setError] = useState(null)
   const isFirstInput = useRef(true)
 
-  console.log('Search =', search)
-  console.log('isFirstInput =', isFirstInput.current)
+  // console.log('Search =', search)
+  // console.log('isFirstInput =', isFirstInput.current)
 
   // This functions returns the search value comming from input when the submit bottom is pressed
 
   // Every time the search value changes when submit button is clicked, this "form" is evaluated
   useEffect(() => {
     if (isFirstInput.current) {
-      isFirstInput.current = false
-      console.log('isFirstInput inside useEffect =', isFirstInput.current)
+      isFirstInput.current = search === ''
+      // console.log('isFirstInput inside useEffect =', isFirstInput.current)
       return
     }
 
@@ -37,19 +38,15 @@ function useSearch () {
   return { search, setSearch, error }
 }
 
-// function useMovies ({ search }) {
-//   console.log(search)
-// }
-
-export default function App () {
-  const { search, setSearch, error } = useSearch()
-
-  const [responseMovies, setResponseMovies] = useState([])
-
-  const inputRef = useRef() // this is just used as an example than a value from input can be taken using useRef
+function useMovies ({ search, sort }) {
+  const [movies, setMovies] = useState([])
+  const [loading, setLoading] = useState(false)
+  const previousSearch = useRef()
+  // console.log('useState 1', search)
+  // console.log('useRef 1 ', previousSearch.current)
 
   const fetchMovies = async () => {
-    if (search === null) return []
+    if (search === '') return []
 
     try {
       const API_KEY = '4287ad07'
@@ -57,32 +54,81 @@ export default function App () {
       const response = await fetch(URL)
       const data = await response.json()
       const moviesFetched = data.Search
-      setResponseMovies(moviesFetched)
+      return moviesFetched
     } catch (error) {
       throw new Error('Error searching movies')
     }
   }
 
-  const handleChange = (e) => {
-    const newSearch = e.target.value
-    setSearch(newSearch)
-    fetchMovies()
+  const getMovies = async () => {
+    if (search === previousSearch.current) return
+    try {
+      setLoading(true)
+      previousSearch.current = search
 
-    console.log('Onchange function, search Value', newSearch)
+      const newMovies = await fetchMovies()
+      setMovies(newMovies)
+    } catch (error) {
+      throw new Error('Error during fetching')
+    } finally {
+      setLoading(false)
+    }
   }
 
-  const handleSubmit = async (e) => {
+  useEffect(() => {
+    getMovies()
+  }, [search])
+
+  const sortedMovies = useMemo(() => {
+    console.log('memoSortedmovies')
+    return sort
+      ? [...movies].sort((a, b) => a.Title.localeCompare(b.Title))
+      : movies
+  }, [sort, movies])
+
+  console.log('render', sortedMovies)
+
+  return { movies: sortedMovies, getMovies, loading }
+}
+
+export default function App () {
+  const [sort, setSort] = useState(false)
+
+  const { search, setSearch, error } = useSearch()
+  const { movies, getMovies, loading } = useMovies({ search, sort })
+
+  const inputRef = useRef() // this is just used as an example than a value from input can be taken using useRef
+
+  const debouncedGetMovies = useCallback(
+    debounce((e) => {
+      const newSearch = e.target.value
+      setSearch(newSearch)
+      console.log('debounce')
+      getMovies()
+    }, 2500)
+  )
+
+  const handleChange = (e) => {
+    debouncedGetMovies(e)
+
+    // console.log('Onchange function, search Value', newSearch)
+  }
+
+  const handleSubmit = (e) => {
     e.preventDefault()
-    fetchMovies()
+    const value = inputRef.current.value
+    setSearch(value)
+    getMovies()
 
     const data = new FormData(e.target)
     data.get('query')
     // console.log('From Form Data ', data.get('query'))
 
-    const value = inputRef.current.value
     // console.log('Search Value when submit button click =', value)
+  }
 
-    return setSearch(value)
+  const handleSort = () => {
+    setSort(!sort)
   }
 
   return (
@@ -94,7 +140,7 @@ export default function App () {
               ref={inputRef}
               onChange={handleChange}
               placeholder="Avatar, Fast & Furious ..." />
-        <input name='checkbox' type="checkbox" />
+        <input name='checkbox' onChange={handleSort} type="checkbox" checked={sort}/>
         <button type="submit" >SEARCH</button>
       </form>
       {error && <p style={{ color: 'red' }}>{error}</p>}
@@ -102,10 +148,11 @@ export default function App () {
 
     <main>
       <ul>
-
-        {responseMovies?.length > 1
+      {loading
+        ? <p>Loading data.......</p>
+        : movies?.length > 1
         // Pending Create a component of movies
-          ? responseMovies.map((result) => (
+          ? movies.map((result) => (
             <li className='movie'
                 key={result.imdbID}>
                 <h3>{result.Title}</h3>
@@ -114,7 +161,7 @@ export default function App () {
             </li>
           ))
           : noResponseJSON.Error
-        }
+      }
       </ul>
     </main>
   </div>
